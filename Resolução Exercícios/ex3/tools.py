4d7cb6872465262d4dc1fdaffc273832879ca2a3
ianamaral12 <ianfelipeamaral2003@gmail.com>
2026-03-10 20:19:54 -0300

    feat: ex3

---
+++ Resolução Exercícios/ex3/tools.py
@@ -0,0 +1,5 @@
+def celsius_para_fahrenheit(temperatura_original):
+    return (temperatura_original * 9 / 5) + 32
+
+def fahrenheit_para_celsius(temperatura_original):
+    return (temperatura_original - 32) * 5 / 9
